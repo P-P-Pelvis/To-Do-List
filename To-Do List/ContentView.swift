58d35220b601b631044fde391b061b7f13da8cf2
Edwin Tovar <poohwinnie2800@gmail.com>
2026-01-21 08:35:48 -0800

    Initial Commit

---
+++ To-Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do List
+//
+//  Created by Edwin Tovar on 1/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
